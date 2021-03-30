b462fc873a85fd40801c1632f5dba05b371ad8e6
Joshua Retallick <josh@old-empire.co.uk>
2021-03-30 15:18:10 +0100

    Solution complete

--- lib/leetcode.rb
+++ lib/leetcode.rb
@@ -1,3 +1,17 @@
 def two_sum(nums,target)
-  2
+# looping 0 to length of array (note minus one for indices)
+  (0..nums.length-1).each do |i|
+# nums.length = 4
+# i = 0
+    pointer=i+1
+# pointer = 1
+# looping 1 to 3
+    (pointer..nums.length-1).each do |j|
+      if nums[i] + nums[j] == target
+# nums[i] = 2
+# nums[j] = 7
+        return [i,j]
+      end
+    end
+  end
 end
--- spec/leetcode_spec.rb
+++ spec/leetcode_spec.rb
@@ -1,7 +1,8 @@
 require 'leetcode'
 
 describe "#two_sum" do
-  it "returns [0, 1] if when added together matches target total" do
-    expect(two_sum([1,1],2)).to eq 2
+  it "returns indices [0, 1] if when added together matches target total of 2" do
+    expect(two_sum([2, 7, 11, 15],9)).to eq [0,1]
   end
+
 end
